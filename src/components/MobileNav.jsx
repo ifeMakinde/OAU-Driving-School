2348996b292864a763b2abf997c9dabe0a813bc6
ifeMakinde <makindeifeoluwa07@gmail.com>
2026-01-06 11:54:23 +0100

    fixed build bug

--- src/components/MobileNav.jsx
+++ src/components/MobileNav.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 function MobileNav({ nav }) {
-  const mobileNav = [...nav, "book a session"];
+  const mobileNav = [...nav, "Book a Session"];
 
   return (
-    <div className="w-full flex-col justify-center items-center absolute top-20 right-30 left-3 bg-white text-center p-6">
+    <div className="w-full flex-col justify-center items-center absolute top-18 right-30 left-3 bg-white text-center p-6">
       {mobileNav.map((nav) => (
         <li
           key={nav}
